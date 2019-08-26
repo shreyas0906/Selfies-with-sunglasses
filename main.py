eca735c21535507a54117bad2ce51596c71a1b3b
shreyas0906 <shreyas0906@gmail.com>
2019-08-26 09:49:04 -0700

    fixed issue with draw function

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ def processDir(dirName, fileList):
 def startProcessDir(dirName, imageList):
 
     checkAndCreateSaveDir(p)
-
+    global image
     for fileName in imageList:
 
         image = cv2.imread(os.path.join(dirName, fileName))
         imageClone = image.copy()
         cv2.namedWindow(fileName)
-        cv2.setMouseCallback(image, clickDrag)
+        cv2.setMouseCallback(fileName, clickDrag)
 
         while True:
 
@@ -37,25 +37,19 @@ def startProcessDir(dirName, imageList):
             if key == ord("n"):
                 saveCordinates(fileName)
                 cv2.destroyWindow(fileName)
-                break
+                continue
 
             elif key == ord("r"):
                 image = imageClone
-                overallRect.remove(refPt)
-                global refPt
+                cv2.setMouseCallback(fileName, clickDrag)
 
-def saveCordinates(fileName):
-    
-    saveDir = p.saveDir + 'coordinates'
-    textName = fileName.split('.')[0] + '.txt'
-    
-    with open(saveDir + '/' + textName, 'w') as f:
-        f.write(overallRect +'\n')
-        f.write(overallCircle)
+            elif key == ord("q"):
+                cv2.destroyWindow(fileName)
+                break
 
 def checkAndCreateSaveDir(p):
 
-    saveCordinatesDir = p.saveDir + 'coordinates'
+    saveCordinatesDir = p.saveDir + 'Coordinates'
     
     if not p.saveDir in os.listdir(os.getcwd()):
         os.makedirs(os.path.join(os.getcwd(), p.saveDir))
@@ -66,66 +60,29 @@ def checkAndCreateSaveDir(p):
 
 # Listening for the mouse events
 def clickDrag(event, x, y, flags, param):
-    global refPt, cropping, overallCircle, overallRect
+
+    global refPt, centrePt, overallCircle, overallRect
+
     if event == cv2.EVENT_RBUTTONDBLCLK:
         cv2.circle(image, (x, y), 2, (0, 0, 255), -2)
-        # centrePt.append((x, y))
-        overallCircle.append(centrePt)
+        overallCircle.append((x,y))
 
     elif event == cv2.EVENT_LBUTTONDOWN:
-        refPt = [(x, y)]
-        cropping = True
+        refPt = [(x,y)]
+        print("refPt: {}".format(refPt))
 
     elif event == cv2.EVENT_LBUTTONUP:
-        refPt.append((x, y))
-        overallRect.append(refPt)
-        cropping = False
-    cv2.rectangle(image, refPt[0], refPt[1], (255, 255, 255), 2)
+        refPt.append((x,y))
+        cv2.rectangle(image, refPt[0], refPt[1], (255, 255, 255), 1)
 
+def saveCordinates(fileName):
+    saveDir = p.saveDir + 'coordinates'
+    textName = fileName.split('.')[0] + '.txt'
 
+    with open(saveDir + '/' + textName, 'w') as f:
+        f.write(overallRect + '\n')
+        f.write(overallCircle)
 
-
-
-for i in file_images:
-
-    file_name = i.split('.')
-    save_name = file_name[0]
-    image = cv2.imread(i)
-    cv2.imshow(i, image)
-    clone = image.copy()
-    cv2.namedWindow(save_name)
-    cv2.setMouseCallback(i, clickDrag)
-
-    while True
-        cv2.imshow(i, image)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("n"):
-            # refPt = []
-            save_file[str(save_name)] = centrePt
-            # centrePt = []
-
-            cv2.destroyWindow(i)
-            break
-        elif key == ord("r"):
-            image = clone.copy()
-            overallRect.remove(refPt)
-            refPt = []
-            centrePt = []
-
-    cv2.namedWindow("ROI")
-    roi = clone[overallRect[j][0][1]:overallRect[j][1][1], overallRect[j][0][0]:overallRect[j][1][0]]
-    cv2.imshow("ROI", roi)
-    os.chdir(save_path)
-    cv2.imwrite(save_name + extension, roi)
-    os.chdir(mypath)
-    cv2.waitKey(0)
-    cv2.destroyWindow("ROI")
-    j += 1
-
-def checkCreateDir():
-
-    if not p.saveDir in os.listdir(os.getcwd()):
-        os.makedirs(os.getcwd() + '/' + p.saveDir)
 
 
 if __name__ == '__main__':
